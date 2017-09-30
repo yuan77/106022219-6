74c65e9db322a0244f8c58fcd03b263c86efdb0d
yuan77 <sky58029@gmail.com>
2017-09-30 15:05:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       Scanner in=new Scanner(System.in);
+       int x,y,z,a;
+       x=in.nextInt();
+       y=in.nextInt();
+       z=in.nextInt();
+       System.out.println(x+y+z);
+       System.out.println((x+y+z)/3);
+        // write your code here
+    }
+}
+
